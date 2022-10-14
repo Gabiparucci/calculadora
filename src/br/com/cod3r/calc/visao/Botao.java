040509d079ec48c91e7a93e61d10454b00ea6aec
gabiParucci <gabispachi.parucci@gmail.com>
2022-10-14 09:48:08 -0300

    first commit

---
+++ src/br/com/cod3r/calc/visao/Botao.java
@@ -0,0 +1,17 @@
+package br.com.cod3r.calc.visao;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton {
+
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setFont(new Font("courier", Font.PLAIN, 25));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setForeground(Color.WHITE);
+    }
+}
